57888b18a1a615892ffc7d8d005001cd5db9d89d
Eugenio Sales <eugenio.sales@outlook.com>
2022-03-08 22:20:14 -0300

    Adiçao de variavel de ambiente de homologaçao

--- puma/src/config/environment.js
+++ puma/src/config/environment.js
@@ -3,6 +3,9 @@ module.exports = {
     if (process.env.VUE_APP_ENVIRONMENT === 'dev') {
       /* eslint-disable  semi */
       global.URL_GATEWAY = `http://${process.env.VUE_APP_IP_ADDRESS}:3004`
+    } else if (process.env.VUE_APP_ENVIRONMENT === 'test') {
+      /* eslint-disable  semi */
+      global.URL_GATEWAY = `${process.env.VUE_APP_IP_ADDRESS}`
     } else if (process.env.VUE_APP_ENVIRONMENT === 'prod') {
       /* eslint-disable  semi */
       global.URL_GATEWAY = `http://${process.env.VUE_APP_IP_ADDRESS}:3004`
