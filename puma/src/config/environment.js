3f830479a607177d84302f25cf4464cb959f7107
Mexazonic <brunnohenriq697@gmail.com>
2022-05-06 02:45:38 -0300

    Adição de ambiente hom (homologação) Front

--- puma/src/config/environment.js
+++ puma/src/config/environment.js
@@ -3,13 +3,13 @@
 module.exports = {
   configUser: () => {
     if (process.env.VUE_APP_ENVIRONMENT === 'dev') {
-      global.URL_GATEWAY = http://${process.env.VUE_APP_IP_ADDRESS}:3004;
+      global.URL_GATEWAY = `http://${process.env.VUE_APP_IP_ADDRESS}:3004`;
     } else if (process.env.VUE_APP_ENVIRONMENT === 'test') {
-      global.URL_GATEWAY = http://${process.env.VUE_APP_IP_ADDRESS}:3004;
+      global.URL_GATEWAY = `http://${process.env.VUE_APP_IP_ADDRESS}:3004`;
     } else if (process.env.VUE_APP_ENVIRONMENT === 'hom') {
-      global.URL_GATEWAY = ${process.env.VUE_APP_IP_ADDRESS};
+      global.URL_GATEWAY = `${process.env.VUE_APP_IP_ADDRESS}`;
     } else if (process.env.VUE_APP_ENVIRONMENT === 'prod') {
-      global.URL_GATEWAY = http://${process.env.VUE_APP_IP_ADDRESS}:3004;
+      global.URL_GATEWAY = `http://${process.env.VUE_APP_IP_ADDRESS}:3004`;
     }
   },
 };
