98a0760ce3a8dbd78ede05552a1ff4862aeabbc9
Mexazonic <brunnohenriq697@gmail.com>
2022-05-06 02:30:24 -0300

    Adição de configuração ambiente pra homologação - Frontend

--- puma/src/config/environment.js
+++ puma/src/config/environment.js
@@ -1,14 +1,15 @@
+/* eslint-disable */
+
 module.exports = {
   configUser: () => {
     if (process.env.VUE_APP_ENVIRONMENT === 'dev') {
-      /* eslint-disable  semi */
-      global.URL_GATEWAY = `http://${process.env.VUE_APP_IP_ADDRESS}:3004`
+      global.URL_GATEWAY = http://${process.env.VUE_APP_IP_ADDRESS}:3004;
     } else if (process.env.VUE_APP_ENVIRONMENT === 'test') {
-      /* eslint-disable  semi */
-      global.URL_GATEWAY = `${process.env.VUE_APP_IP_ADDRESS}`
+      global.URL_GATEWAY = http://${process.env.VUE_APP_IP_ADDRESS}:3004;
+    } else if (process.env.VUE_APP_ENVIRONMENT === 'hom') {
+      global.URL_GATEWAY = ${process.env.VUE_APP_IP_ADDRESS};
     } else if (process.env.VUE_APP_ENVIRONMENT === 'prod') {
-      /* eslint-disable  semi */
-      global.URL_GATEWAY = `http://${process.env.VUE_APP_IP_ADDRESS}:3004`
+      global.URL_GATEWAY = http://${process.env.VUE_APP_IP_ADDRESS}:3004;
     }
   },
 };
