6c3e4d436efe7014dfbe930ebe00e999047264cf
giovannabbottino <giovannabbottino@gmail.com>
2022-08-19 19:52:18 -0300

    (#128) atualiza test

--- puma/tests/unit/components/nav-bar/nav-bar.spec.js
+++ puma/tests/unit/components/nav-bar/nav-bar.spec.js
@@ -1,12 +1,12 @@
-import {createLocalVue } from '@vue/test-utils'
-import CompAPI from '@vue/composition-api'
+import { createLocalVue, mount } from '@vue/test-utils';
+import CompAPI from '@vue/composition-api';
 
-import NavBar from '../../../../src/components/shared/nav-bar/NavBar.vue'
+import NavBar from '../../../../src/components/shared/nav-bar/NavBar.vue';
 
 describe('Mounted NavBar', () => {
-    const wrapper = mount(NavBar);
-  
-    test('does a wrapper exist', () => {
-      expect(wrapper.exists()).toBe(true)
-    });
+  const wrapper = mount(NavBar);
+
+  test('does a wrapper exist', () => {
+    expect(wrapper.exists()).toBe(true);
   });
+});
