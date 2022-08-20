716de7d8fadeaea11dc45c26b07e2ec95bb1d7f5
Bruno-Felix <balvesfelix@gmail.com>
2022-08-20 15:24:28 -0300

    teste tabela disciplinas com coluna professores

--- puma/src/components/disciplina/consulta-disciplina/ConsultaDisciplina.vue
+++ puma/src/components/disciplina/consulta-disciplina/ConsultaDisciplina.vue
@@ -1,5 +1,147 @@
-<template src="./consulta-disciplina.html"></template>
+<template>
+    <div class="main-content hidden-scroll-bar">
+        <h1 class="tittle ml-4">
+            Disciplinas
+        </h1>
 
-<script src="./consulta-disciplina.js"></script>
+        <div class="tittle sub-tittle ml-4">
+            Disciplinas Cadastradas na Plataforma
+        </div>
+
+        <div>
+            <button
+                class="btn cd-btn ml-4 my-3"
+                onclick="window.location.href = '/disciplinas/cadastrar'">
+                    <i class="fa-solid fa-plus-circle mr-2 add-project"></i>Criar Disciplina
+            </button>
+        </div>
+
+        <ListagemConsultaDisciplinaComponent :title="'Minhas Disciplinas'" :list="mySubjects"/>
+        <ListagemConsultaDisciplinaComponent :title="'Demais Disciplinas'" :list="auxsubjects"/>
+    </div>
+</template>
+
+<script>
+import SubjectService from '../../../services/SubjectService';
+import ListagemConsultaDisciplinaComponent from './ListagemConsultaDisciplinaComponent.vue';
+
+export default {
+  beforeMount() {
+    this.getSubjects();
+  },
+
+  components: {
+    ListagemConsultaDisciplinaComponent,
+  },
+
+  data: () => ({
+    isLoading: false,
+    wasLoaded: false,
+    isDeletingSubject: false,
+    subjects: [],
+    mySubjects: [],
+    auxsubjects: [
+      {
+        name: 'Calculo23',
+        subjectid: 91,
+        professors: [
+          {
+            email: 'giovannabbottino@gmail.com',
+            fullname: 'Giovanna Borges Bottino',
+            regnumber: '1700112',
+            userid: 60,
+          },
+          {
+            email: 'giovannabbottino@gmail.com',
+            fullname: 'Giovanna Borges Bottino',
+            regnumber: '1700112',
+            userid: 61,
+          },
+        ],
+      },
+      {
+        name: 'Calculo34',
+        subjectid: 92,
+        professors: [
+          {
+            email: 'giovannabbottino@gmail.com',
+            fullname: 'Giovanna Borges Bottino',
+            regnumber: '1700112',
+            userid: 62,
+          },
+        ],
+      },
+      {
+        name: 'Calculo4',
+        subjectid: 93,
+        professors: [
+          {
+            email: 'giovannabbottino@gmail.com',
+            fullname: 'Giovanna Borges Bottino',
+            regnumber: '1700112',
+            userid: 4,
+          },
+        ],
+      },
+      {
+        name: 'Calculo3',
+        subjectid: 94,
+        professors: [
+          {
+            email: 'giovannabbottino@gmail.com',
+            fullname: 'Giovanna Borges Bottino',
+            regnumber: '1700112',
+            userid: 4,
+          },
+        ],
+      },
+      {
+        name: 'Calculo2',
+        subjectid: 95,
+        professors: [
+          {
+            email: 'giovannabbottino@gmail.com',
+            fullname: 'Giovanna Borges Bottino',
+            regnumber: '1700112',
+            userid: 4,
+          },
+        ],
+      },
+    ],
+    subjectService: new SubjectService(),
+  }),
+
+  methods: {
+    getSubjects() {
+      this.$store.commit('OPEN_LOADING_MODAL', { title: 'Carregando...' });
+      this.subjectService.getSubjects().then((response) => {
+        this.subjects = response.data;
+        this.$store.commit('CLOSE_LOADING_MODAL');
+        this.separateSubjects();
+      }).catch(() => {
+        this.$store.commit('CLOSE_LOADING_MODAL');
+        this.makeToast('ERRO', 'Erro ao recuperar disciplinas', 'danger');
+      });
+    },
+    separateSubjects() {
+      this.auxsubjects.map((sub) => {
+        sub.professors.map((prof) => {
+          console.log(prof.userid, this.$store.getters.user.userId);
+          if (prof.userid === this.$store.getters.user.userId) {
+            this.mySubjects.push(sub);
+            this.auxsubjects = this.auxsubjects.filter((item) => (
+              item.subjectid !== sub.subjectid));
+          }
+          return prof;
+        });
+        return null;
+      });
+    },
+    goToSubject(subjectid) {
+      this.$router.push({ path: `/disciplinas/visualizar/${subjectid}` });
+    },
+  },
+};
+</script>
 
 <style src="./consulta-disciplina.css"></style>
---
+++ puma/src/components/disciplina/consulta-disciplina/ListagemConsultaDisciplinaComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="mt-4">
+        <h2 class="tittle sub-tittle ml-4">{{ title }}</h2>
+        <table>
+            <thead>
+                <tr>
+                    <td>Título</td>
+                    <td>Professorres</td>
+                    <td></td>
+                </tr>
+            </thead>
+            <tr v-for="subject in list" :key="subject.subjectid">
+                <td>{{subject.name}}</td>
+                <td>
+                    <div v-for="professor in subject.professors" :key="professor.userid">
+                        {{professor.fullname}}
+                    </div>
+                </td>
+                <td>
+                    <button
+                    class="btn cd-btn"
+                    @click="goToSubject(subject.subjectid)">
+                    <i class="fa-solid fa-circle-info mr-2 ml-0"></i>Ver Detalhes
+                    </button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    list: Array,
+  },
+};
+</script>
