c6c21f518980c06cc78f4ef3ae5909ce49e8aef6
gabriellcabrall <gabrielbielcabral@gmail.com>
2022-08-21 17:35:18 -0300

    (#98) Adiciona caixa de pesquisa de disciplina

--- puma/src/components/disciplina/consulta-disciplina/ConsultaDisciplina.vue
+++ puma/src/components/disciplina/consulta-disciplina/ConsultaDisciplina.vue
@@ -1,27 +1,40 @@
 <template>
-    <div class="main-content hidden-scroll-bar">
-        <h1 class="tittle ml-4">
-            Disciplinas
-        </h1>
+  <div class="main-content hidden-scroll-bar">
+    <h1 class="tittle ml-4">
+      Disciplinas
+    </h1>
 
-        <div class="tittle sub-tittle ml-4">
-            Disciplinas Cadastradas na Plataforma
-        </div>
-
-        <div>
-            <button
-                class="btn cd-btn ml-4 my-3"
-                onclick="window.location.href = '/disciplinas/cadastrar'">
-                <i class="fa-solid fa-plus-square mr-2 add-project"></i>Criar Disciplina
-            </button>
-        </div>
-
-        <div class="tabelas">
-          <ListagemConsultaDisciplinaComponent class="minhasDisciplinas" :list="mySubjects"/>
-          <hr class="mb-0 mt-5">
-          <ListagemConsultaDisciplinaComponent class="demaisDisciplinas" :list="subjects"/>
-        </div>
+    <div class="tittle sub-tittle ml-4">
+      Disciplinas Cadastradas na Plataforma
     </div>
+
+    <div class="ml-4 input-group">
+      <input
+        onclick="pesquisar(this.value)"
+        type="text"
+        id="caixaPesquisa"
+        class="search-input"
+        placeholder="Pesquise por uma disciplina">
+      <i class="fas fa-search input-group-prepend search-icon"></i>
+      <div v-if="!subjects.length" class="no-results align-content-center mt-3">
+        Sem resultados disponíveis
+      </div>
+    </div>
+
+    <div>
+      <button
+        class="btn cd-btn ml-4 my-3"
+        onclick="window.location.href = '/disciplinas/cadastrar'">
+        <i class="fa-solid fa-plus-square mr-2 add-project"></i>Criar Disciplina
+      </button>
+    </div>
+
+    <div class="tabelas">
+      <ListagemConsultaDisciplinaComponent class="minhasDisciplinas" :list="mySubjects" />
+      <hr class="mb-0 mt-5">
+      <ListagemConsultaDisciplinaComponent class="demaisDisciplinas" :list="subjects" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -79,4 +92,5 @@ export default {
 };
 </script>
 
-<style src="./consulta-disciplina.css"></style>
+<style src="./consulta-disciplina.css">
+</style>
