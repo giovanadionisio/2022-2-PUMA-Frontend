05a7f04bff6b3e73e04c368550fb34ec537621b5
giovannabbottino <giovannabbottino@gmail.com>
2022-08-22 15:44:24 -0300

    (#128) adiciona mais nav bar tests

--- puma/tests/unit/components/nav-bar/nav-bar.spec.js
+++ puma/tests/unit/components/nav-bar/nav-bar.spec.js
@@ -1,12 +1,28 @@
-import { createLocalVue, mount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
 import CompAPI from '@vue/composition-api';
 
 import NavBar from '../../../../src/components/shared/nav-bar/NavBar.vue';
 
 describe('Mounted NavBar', () => {
-  const wrapper = mount(NavBar);
+  const wrapper = shallowMount(NavBar, {
+    propsData : {
+      title   : 'title',
+      subtitle: 'subtitle',
+      username: 'username'
+    },
+  });
 
-  test('does a wrapper exist', () => {
+  it('does a wrapper exist', () => {
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('does the title is shows as the value', () => {
+    expect(wrapper.find('.title').text()).toEqual('title')
+  });
+  it('does the sub-title is shows as the value', () => {
+    expect(wrapper.find('.sub-title').text()).toEqual('subtitle')
+  });
+  it('does the user is shows as the value', () => {
+    expect(wrapper.find('.user').text()).toEqual('username')
   });
 });
