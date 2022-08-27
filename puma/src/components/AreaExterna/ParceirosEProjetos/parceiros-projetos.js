48b355ac0e986eed9ce5305dc304173e9dbd5d64
bhsouza <breno.hs@outlook.com>
2022-08-27 17:14:13 -0300

    (#117) Inicia componente InfoParceiro
    Co-authored-by: giovannabbottino <giovannabbottino@gmail.com>
    Co-authored-by: FelipeDireito <fedireito92@gmail.com>

--- puma/src/components/AreaExterna/ParceirosEProjetos/parceiros-projetos.js
+++ puma/src/components/AreaExterna/ParceirosEProjetos/parceiros-projetos.js
@@ -1,12 +1,22 @@
 import AreaExternaHeader from '../AreaExternaHeader/AreaExternaHeader.vue';
+import InfoParceiro from '../InfoParceiro/InfoParceiro.vue';
 /* eslint-disable*/
 export default {
   components: {
     AreaExternaHeader,
+    InfoParceiro,
   },
 
   data() {
     return {
+      infoParceiros: [{
+        name: 'Exército Brasileiro',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores obcaecati perspiciatis ut omnis sunt vel inventore corporis! Id ad, aliquam voluptatem ipsam nobis corporis accusamus perferendis dolor magnam nihil.Nobis corporis, quae totam nam pariatur eaque voluptatibus veritatis exercitationem assumenda, nesciunt illo? Magnam labore sapiente pariatur quisquam praesentium ea eveniet distinctio nihil modi. Deleniti perferendis esse voluptatem quam molestiae.',
+        website: 'https://www.google.com',
+        projectScope: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores obcaecati perspiciatis ut omnis sunt vel inventore corporis! Id ad, aliquam voluptatem ipsam nobis corporis accusamus perferendis dolor magnam nihil.Nobis corporis, quae totam nam pariatur eaque voluptatibus veritatis exercitationem assumenda, nesciunt illo? Magnam labore sapiente pariatur quisquam praesentium ea eveniet distinctio nihil modi. Deleniti perferendis esse voluptatem quam molestiae.',
+        projectKeyResults: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores obcaecati perspiciatis ut omnis sunt vel inventore corporis! Id ad, aliquam voluptatem ipsam nobis corporis accusamus perferendis dolor magnam nihil.Nobis corporis, quae totam nam pariatur eaque voluptatibus veritatis exercitationem assumenda, nesciunt illo? Magnam labore sapiente pariatur quisquam praesentium ea eveniet distinctio nihil modi. Deleniti perferendis esse voluptatem quam molestiae.',
+        logo: 'https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png',
+      }],
       paginaAtual: '/home/parceiros',
     };
   },
