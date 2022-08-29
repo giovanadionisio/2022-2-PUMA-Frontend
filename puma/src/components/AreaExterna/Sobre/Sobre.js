429140f1e0fb23d6522a42954d942f61d37aee35
Bruno-Felix <balvesfelix@gmail.com>
2022-08-29 18:07:56 -0300

    (#114) Criando função showInformacao para a visualização seletiva das Informações

--- puma/src/components/AreaExterna/Sobre/Sobre.js
+++ puma/src/components/AreaExterna/Sobre/Sobre.js
@@ -10,4 +10,22 @@ export default {
       paginaAtual: '/home/sobre',
     };
   },
+
+  methods: {
+    showInformacao(element) {
+      const content = document.getElementById(element);
+
+      if (!content.style.display) {
+        if (element === 'primeira-informacao') {
+          content.style.display = 'none';
+        } else {
+          content.style.display = 'block';
+        }
+      } else if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    },
+  },
 };
