a36c5022724ce22d27e273580190c129e97d7e93
Bruno-Felix <balvesfelix@gmail.com>
2022-08-30 19:05:44 -0300

    (#109) Criando base component ObjetoDisciplina

--- puma/src/components/AreaExterna/Disciplinas/Disciplina.js
+++ puma/src/components/AreaExterna/Disciplinas/Disciplina.js
@@ -1,8 +1,10 @@
 import AreaExternaHeader from '../AreaExternaHeader/AreaExternaHeader.vue';
+import ObjetoDisciplina from './ObjetoDisciplina/ObjetoDisciplina.vue';
 
 export default {
   components: {
     AreaExternaHeader,
+    ObjetoDisciplina,
   },
 
   data() {
