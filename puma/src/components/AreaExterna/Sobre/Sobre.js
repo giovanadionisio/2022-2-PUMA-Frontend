e84ea25b1420b92f6327241f6c127802d474aa00
Bruno-Felix <balvesfelix@gmail.com>
2022-08-30 09:45:33 -0300

    (#114) Retirando condição função showInformacao()

--- puma/src/components/AreaExterna/Sobre/Sobre.js
+++ puma/src/components/AreaExterna/Sobre/Sobre.js
@@ -16,11 +16,7 @@ export default {
       const content = document.getElementById(element);
 
       if (!content.style.display) {
-        if (element === 'primeira-informacao') {
-          content.style.display = 'none';
-        } else {
-          content.style.display = 'block';
-        }
+        content.style.display = 'block';
       } else if (content.style.display === 'block') {
         content.style.display = 'none';
       } else {
