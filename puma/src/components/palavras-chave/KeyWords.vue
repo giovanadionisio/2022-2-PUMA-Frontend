7fab9c997c6f842214c92ed7e91178fdb965f6b9
Breno Henrique <breno.hs@outlook.com>
2022-08-31 19:25:24 -0300

    (#144) Inicia refatoracao da listagem das palavras-chave
    Co-authored-by: enzoggqs <eggqsaraiva@gmail.com>

--- puma/src/components/palavras-chave/KeyWords.vue
+++ puma/src/components/palavras-chave/KeyWords.vue
@@ -1,5 +1,37 @@
-<template src="./keyWords.html"></template>
+<template>
+  <div class="main-content hidden-scroll-bar">
+    <h1 class="kw-title ml-4">
+      Palavras-Chave
+    </h1>
 
-<script src="./keyWords.js"></script>
+    <div class="kw-sub-title ml-4">
+      PALAVRAS-CHAVE CADASTRADAS NA PLATAFORMA
+    </div>
+
+    <div class="div-filter-and-button ml-4">
+      <b-form-select @input="filter" class="col-md-4 select" v-model="selected" :options="subjects">
+      </b-form-select>
+      <button class="kw-btn btn sizes mr-2" >
+        <i class="fa-solid fa-plus-circle ml-3 add-project"></i>
+        ADICIONAR PALAVRA-CHAVE
+      </button>
+    </div>
+
+    <div class="tabelas">
+      <ListagemConsultaKeyWords />
+
+    </div>
+  </div>
+</template>
+
+<script>
+import ListagemConsultaKeyWords from './ListagemConsultaKeyWords.vue';
+
+export default {
+  components: {
+    ListagemConsultaKeyWords,
+  },
+};
+</script>
 
 <style src="./keyWords.css"></style>
---
+++ puma/src/components/palavras-chave/ListagemConsultaKeyWords.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="mt-4 ml-4">
+        <table>
+            <thead>
+                <tr>
+                    <td>ITEM</td>
+                    <td>PALAVRA-CHAVE</td>
+                    <td>DISCIPLINA</td>
+                    <td></td>
+                </tr>
+            </thead>
+
+            <tr>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+                <td>
+                  <button class="btn cd-btn mx-2">
+                    <font-awesome-icon icon="fas fa-edit" size="lg" />
+                    EDITAR
+                  </button>
+                  <button class="btn cd-btn mx-2">
+                    <font-awesome-icon icon="fa-solid fa-trash" size="lg" />
+                    EXCLUIR
+                  </button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+
+</script>
