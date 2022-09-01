11d6c5bcbb14d5a1f85f4148c067ac03ae97bded
Bruno-Felix <balvesfelix@gmail.com>
2022-08-31 21:50:33 -0300

    Padronizando refatorando página Contatos

--- puma/src/components/AreaExterna/Contato/Contato.js
+++ puma/src/components/AreaExterna/Contato/Contato.js
@@ -8,6 +8,23 @@ export default {
   data() {
     return {
       pagina: '/Contato',
+      contatos: [
+        {
+          nome: 'Departamento de Engenharia de Produção',
+          endereco: 'Sala DT-77/15, prédio da FT, bloco D UnB - Campus Universitário Darcy Ribeiro, Brasília - DF',
+          email: 'emaildopuma@unb.br',
+        },
+        {
+          nome: 'Administrador da plataforma 1',
+          endereco: '',
+          email: '121234567@unb.br',
+        },
+        {
+          nome: 'Professor responsavel 1',
+          endereco: '',
+          email: '981234567@unb.br',
+        },
+      ],
     };
   },
 };
